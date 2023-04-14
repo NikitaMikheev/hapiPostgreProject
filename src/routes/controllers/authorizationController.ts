ca732a0c9f8e674eb0f61319bc9176dd656bac2f
Nikita Mikheev <nikita.miheev.1999@mail.ru>
2023-04-15 00:51:23 +0300

    add eslint and prettier

--- src/routes/controllers/authorizationController.ts
+++ src/routes/controllers/authorizationController.ts
@@ -1,68 +1,64 @@
-import { validateRefresh } from "../../model/service/tokenServiceValidateRefresh";
-import { validate } from "../../model/service/tokenServiceValidate";
-import { tokenDelete } from "../../model/service/tokenServiceDelete";
-import { functionPost } from "../../model/service/userServicePost";
-import { myUser, Tokens } from "../../types/type";
+import { validateRefresh } from '../../model/service/tokenServiceValidateRefresh';
+import { validate } from '../../model/service/tokenServiceValidate';
+import { tokenDelete } from '../../model/service/tokenServiceDelete';
+import { functionPost } from '../../model/service/userServicePost';
+import { myUser, Tokens } from '../../types/type';
 
-export const handlerRegister = async (request, h):Promise<false | Tokens> => { // добавление пользователя через форму 
-    try {
+export const handlerRegister = async (request, h): Promise<false | Tokens> => {
+  // добавление пользователя через форму
+  try {
 
-        let formObj:myUser = {
-            firstName: request.payload.userName,
-            lastName: request.payload.userLastName,
-            userEmail: request.payload.userEmail,
-            userPass: request.payload.userPass,
-            userPassConfm: request.payload.userPassConfm,
-            age: parseInt(request.payload.userAge),
-            city: request.payload.userCity
-        }
-    
-        const res = await functionPost(formObj); // пользователь добавлен в бд
+    let formObj: myUser = {
+      firstName: request.payload.userName,
+      lastName: request.payload.userLastName,
+      userEmail: request.payload.userEmail,
+      userPass: request.payload.userPass,
+      userPassConfm: request.payload.userPassConfm,
+      age: parseInt(request.payload.userAge),
+      city: request.payload.userCity,
+    };
 
-        if(res===false) {
-            return false;
-        }
-        const token:false | Tokens = await validate(formObj.userEmail, formObj.userPass); // проверяем и возвращаем сразу 2 токена
+    const res = await functionPost(formObj); // пользователь добавлен в бд
 
-        
-        if(token) {
-            h.state('refreshToken', {refreshToken: token.refreshToken}) // сохраняем в куки рефреш токен
-        }
+    if (res === false) {
+      return false;
+    }
+    const token: false | Tokens = await validate(formObj.userEmail, formObj.userPass); // проверяем и возвращаем сразу 2 токена
 
-        return token;
+    if (token) {
+      h.state('refreshToken', { refreshToken: token.refreshToken }); // сохраняем в куки рефреш токен
     }
 
-    catch(error) {
-        console.log('Ошибка POST запроса');
-        console.log(error);
-    }
-}
+    return token;
+  } catch (error) {
+    console.log('Ошибка POST запроса');
+    console.log(error);
+  }
+};
 
-export const handlerAuthentication = async (request, h):Promise<false | Tokens> => {
+export const handlerAuthentication = async (request, h): Promise<false | Tokens> => {
+  const token: false | Tokens = await validate(request.payload.email, request.payload.password);
+  await h.unstate('refreshToken'); // очищаем куки (на случай, если остался старый токен)
 
-    const token:false | Tokens = await validate(request.payload.email, request.payload.password);
-    await h.unstate('refreshToken') // очищаем куки (на случай, если остался старый токен)
+  if (token) {
+    h.state('refreshToken', { refreshToken: token.refreshToken }); // сохраняем в куки новый рефреш токен
+  }
+  return token; // на клиент возвращается токен. Далее через запрос по маршруту login клиент передает токен в заголовке. Проверял в insomnia, работает
+};
 
-    if(token) {
-        h.state('refreshToken', {refreshToken: token.refreshToken}) // сохраняем в куки новый рефреш токен
-    }
-    return token; // на клиент возвращается токен. Далее через запрос по маршруту login клиент передает токен в заголовке. Проверял в insomnia, работает
-}
+export const handlerRefreshAuthentication = async (request, h): Promise<false | Tokens> => {
+  const token: false | Tokens = await validateRefresh(request.payload.token);
+  await h.unstate('refreshToken'); // очищаем куки (на случай, если остался старый токен)
 
-export const handlerRefreshAuthentication = async (request, h):Promise<false | Tokens> => {
+  if (token) {
+    h.state('refreshToken', { refreshToken: token.refreshToken }); // сохраняем в куки новый рефреш токен
+  }
 
-    const token:false | Tokens = await validateRefresh(request.payload.token);
-    await h.unstate('refreshToken') // очищаем куки (на случай, если остался старый токен)
+  return token; // на клиент возвращается обновленный refrhesh и access токены.
+};
 
-    if(token) {
-        h.state('refreshToken', {refreshToken: token.refreshToken}) // сохраняем в куки новый рефреш токен    
-    }
+export const handlerLogout = async (request, h): Promise<boolean> => {
+  const res = await tokenDelete(request.payload.token);
 
-    return token; // на клиент возвращается обновленный refrhesh и access токены. 
-}
-
-export const handlerLogout = async (request, h):Promise<boolean> => {
-    const res = await tokenDelete(request.payload.token);
-
-    return res;
-}
+  return res;
+};
--- src/routes/controllers/userController.ts
+++ src/routes/controllers/userController.ts
@@ -1,107 +1,92 @@
-import { functionGet, functionGetByCity } from "../../model/service/userServiceGet";
-import { functionPost } from "../../model/service/userServicePost";
-import { functionPut } from "../../model/service/userServicePut";
-import { functionDel } from "../../model/service/userServiceDelete";
-import { myUser } from "../../types/type";
-import { User } from "../../model/entity/User";
+import { functionGet, functionGetByCity } from '../../model/service/userServiceGet';
+import { functionPost } from '../../model/service/userServicePost';
+import { functionPut } from '../../model/service/userServicePut';
+import { functionDel } from '../../model/service/userServiceDelete';
+import { type myUser } from '../../types/type';
+import { type User } from '../../model/entity/User';
 
-export const handlerGet = async (request, h):Promise<string> => {
+export const handlerGet = async (request, h): Promise<string> => {
+  try {
     try {
-        try {
-            const user:myUser = JSON.parse(await functionGet(request.query.id));
-            console.log(`Пользователь получен: ${user.firstName} ${user.lastName}`);
-        
-            return `Имя: ${user.firstName} <br> Фамилия: ${user.lastName} <br> E-mail: ${user.userEmail} <br> Возраст: ${user.age}`;
-        }
-        catch {
-            return 'Такого пользователя не существует!';
-        }
+      const user: myUser = JSON.parse(await functionGet(request.query.id));
+      console.log(`Пользователь получен: ${user.firstName} ${user.lastName}`);
 
+      return `Имя: ${user.firstName} <br> Фамилия: ${user.lastName} <br> E-mail: ${user.userEmail} <br> Возраст: ${user.age}`;
+    } catch {
+      return 'Такого пользователя не существует!';
     }
+  } catch (error) {
+    console.log('Ошибка GET запроса');
+    console.log(error);
+  }
+};
 
-    catch(error) {
-        console.log('Ошибка GET запроса');
-        console.log(error);
-    }
-}
-
-
-export const handlerGetALLByCity = async (request, h):Promise<false | User[]> => {
+export const handlerGetALLByCity = async (request, h): Promise<false | User[]> => {
+  try {
     try {
-        try {
-          const users = await functionGetByCity(request.query.city);
-          return users;  
-        }
-        catch {
-            return false;
-        }
+      const users = await functionGetByCity(request.query.city);
+      return users;
+    } catch {
+      return false;
+    }
+  } catch (error) {
+    console.log('Ошибка GET запроса');
+    console.log(error);
+    return false;
+  }
+};
 
+export const handlerPost = async (request, h): Promise<string> => {
+  // добавление пользователя через форму
+  try {
+    let formObj: myUser = {
+      firstName: request.payload.userName,
+      lastName: request.payload.userLastName,
+      userEmail: request.payload.userEmail,
+      userPass: request.payload.userPass,
+      userPassConfm: request.payload.userPassConfm,
+      age: parseInt(request.payload.userAge),
+      city: request.payload.userCity,
+    };
+
+    const res: boolean = await functionPost(formObj);
+
+    if (res === false) {
+      return 'Пароли не совпадают!';
     }
 
-    catch(error) {
-        console.log('Ошибка GET запроса');
-        console.log(error);
-        return false;
+    return 'Пользователь добавлен!';
+  } catch (error) {
+    console.log('Ошибка POST запроса');
+    console.log(error);
+  }
+};
+
+export const handlerPut = async (request, h): Promise<string> => {
+  try {
+    const ID: number = request.params.id;
+
+    if (await functionPut(ID, request.payload)) {
+      return 'Такого пользователя не существует!';
     }
-}
+    return 'Изменения сохранены!';
+  } catch (error) {
+    console.log('Ошибка PUT запроса');
+    console.log(error);
+  }
+};
 
-
-export const handlerPost = async (request, h):Promise<string> => { // добавление пользователя через форму 
-    try {
-
-        let formObj:myUser = {
-            firstName: request.payload.userName,
-            lastName: request.payload.userLastName,
-            userEmail: request.payload.userEmail,
-            userPass: request.payload.userPass,
-            userPassConfm: request.payload.userPassConfm,
-            age: parseInt(request.payload.userAge),
-            city: request.payload.userCity
-        }
-    
-        const res:boolean = await functionPost(formObj);
-
-        if(res===false) {
-            return 'Пароли не совпадают!';
-        }
-
-        return 'Пользователь добавлен!';
+export const handlerDel = async (request, h): Promise<string> => {
+  // удаляем пользователя по запросу (например, через Insomnia), в качестве параметров (через /) передаем id
+  try {
+    const ID: number = request.params.id;
+    if (await functionDel(ID)) {
+      return 'Такого пользователя не существует!';
     }
-
-    catch(error) {
-        console.log('Ошибка POST запроса');
-        console.log(error);
-    }
-}
-
-export const handlerPut = async (request, h):Promise<string> => {
-    try {
-        const ID:number = request.params.id;
-
-        if(await functionPut(ID, request.payload)) {
-            return 'Такого пользователя не существует!';
-        }
-        return 'Изменения сохранены!';
-    }
-    catch(error) {
-        console.log('Ошибка PUT запроса');
-        console.log(error);
-        
-    }
-}
-
-export const handlerDel = async (request, h):Promise<string> => { // удаляем пользователя по запросу (например, через Insomnia), в качестве параметров (через /) передаем id
-    try {
-        const ID:number = request.params.id;
-        if(await functionDel(ID)) {
-            return 'Такого пользователя не существует!';
-        }
-        console.log("Пользователь удален!");
-        return 'Пользователь удален!';
-    }
-
-    catch(error) {
-        console.log('Ошибка DELETE запроса');
-        console.log(error);
-    }
-}
+    console.log('Пользователь удален!');
+    return 'Пользователь удален!';
+  } catch (error) {
+    console.log('Ошибка DELETE запроса');
+    console.log(error);
+  }
+};
